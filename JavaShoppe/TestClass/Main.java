51a37aaa6883aacb45ab00075f9f337910ebcda0
Eric Whitlow <Eric Whitlow@DESKTOP-KCJASL9>
2022-01-25 17:41:15 -0500

    updated my branch

--- JavaShoppe/TestClass/Main.java
+++ JavaShoppe/TestClass/Main.java
@@ -10,8 +10,21 @@ public class Main {
 		System.out.println("Amars second change");
 		System.out.println("Testing a theory/ Eric");
 		System.out.println("Branch version 2");
-		
-		
+
+		System.out.println("Amars line 13");
+		System.out.println("Eric has made it here");
+		System.out.println("One more test");
+		System.out.println("Hello World");
+		System.out.println("Amars second change");
+		System.out.println("Testing a theory/ Eric");
+		System.out.println("Branch version 2");
+		System.out.println("Amars line 13");
+	
+
+		System.out.println("Joe's branch");
+
+		System.out.println("Im in master");
+
 		
 		
 		System.out.println("Trying something right now");
