095827942251885735104d542059603592242d61
macar <macar@DESKTOP-QTTELE4>
2022-01-25 17:46:50 -0500

    saved with main

--- JavaShoppe/TestClass/Main.java
+++ JavaShoppe/TestClass/Main.java
@@ -11,12 +11,25 @@ public class Main {
 		System.out.println("Testing a theory/ Eric");
 		System.out.println("Branch version 2");
 
+		System.out.println("Amars line 13");
+		System.out.println("Eric has made it here");
+		System.out.println("One more test");
+		System.out.println("Hello World");
+		System.out.println("Amars second change");
+		System.out.println("Testing a theory/ Eric");
+		System.out.println("Branch version 2");
+		System.out.println("Amars line 13");
+	
+
 		System.out.println("Joe's branch");
 
 		System.out.println("Im in master");
 
 		
-		System.out.println("I added a branch, Eric's branch");
+		
+		System.out.println("Trying something right now");
+		System.out.println("Made changes and am now on my branch");
+		System.out.println("One more try with a new branch/ Eric");
 	}
 
 }
