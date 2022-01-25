f75e8ef2baaa6adf856f8b043bf8722bc520c052
amarvugdalic <amarvugdalic@gmail.com>
2022-01-25 17:21:12 -0500

    conflict amar

--- JavaShoppe/TestClass/Main.java
+++ JavaShoppe/TestClass/Main.java
@@ -11,6 +11,14 @@ public class Main {
 		System.out.println("Testing a theory/ Eric");
 		System.out.println("Branch version 2");
 		System.out.println("Amars line 13");
+		System.out.println("Eric has made it here");
+		System.out.println("One more test");
+		System.out.println("Hello World");
+		System.out.println("Amars second change");
+		System.out.println("Testing a theory/ Eric");
+		System.out.println("Branch version 2");
+		System.out.println("Amars line 13");
+		
 	}
 
 }
