89f258c5c649f18ca4e2050bc389c9bf9590f0a7
Eric Whitlow <Eric Whitlow@DESKTOP-KCJASL9>
2022-01-25 16:49:32 -0500

    One more change to main

--- JavaShoppe/TestClass/Main.java
+++ JavaShoppe/TestClass/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Eric has made it here");
+		System.out.println("One more test");
 		System.out.println("Hello World");
 	}
 
